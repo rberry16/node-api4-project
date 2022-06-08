4e4046373272c1a80d516518026b9a25d79cf054
Ryan Berry <rberry16@hotmail.com>
2022-06-08 10:21:27 -0700

    all functionality added

--- api/router.js
+++ api/router.js
@@ -1,8 +1,36 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.json(`we're live!`);
+router.get('/users', (req, res) => {
+    res.json('users array');
+});
+
+router.post('/register', (req, res) => {
+    if (req.body.username === undefined ||
+        req.body.password === undefined ||
+        !req.body.username ||
+        !req.body.password) {
+            res.status(400).json({
+                message: 'username and password are required'
+            });
+        } else {
+            res.status(201).json({
+                message: `welome ${req.body.username} thanks for joining`
+            });
+        }
+});
+
+router.post('/login', (req, res) => {
+    if (req.body.username === undefined ||
+        req.body.password === undefined ||
+        !req.body.username ||
+        !req.body.password) {
+            res.status(400).json({
+                message: 'username and password are required'
+            });
+        } else {
+            res.json({message: `welcome back ${req.body.username}!`});
+        }
 });
 
 module.exports = router;
